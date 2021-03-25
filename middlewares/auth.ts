d8eea4ea4e84bfee9a09f12b17f8a7278e1cbf44
KelvinMitaki <mitakikelvin1@gmail.com>
2021-03-25 21:17:12 +0300

    added patient auth middleware

---
+++ middlewares/auth.ts
@@ -0,0 +1,28 @@
+import { AuthenticationError, ForbiddenError } from "apollo-server-errors";
+import { Request } from "express";
+import jwt from "jsonwebtoken";
+import { UserAttrs } from "../models/User";
+export const patientAuth = (req: Request) => {
+  if (!req.cookies || Object.keys(req.cookies).length === 0) {
+    throw new AuthenticationError("Not authenticated");
+  }
+  let split_token = req.cookies["client_token"];
+
+  if (!split_token) {
+    split_token = req.cookies["token"];
+  }
+  if (!split_token) {
+    throw new AuthenticationError("Not authenticated");
+  }
+
+  try {
+    const user = jwt.verify(split_token, process.env.JWT_SECRET!) as UserAttrs;
+    if (user.isDoctor) {
+      throw new ForbiddenError("Not authorized");
+    }
+    return user;
+  } catch (error) {
+    console.log({ error });
+    throw new AuthenticationError("Not authenticated");
+  }
+};
--- resolvers/mutations/PostMutations.ts
+++ resolvers/mutations/PostMutations.ts
@@ -1,1 +1,14 @@
-export const PostMutations = {};
+import { Context } from "..";
+
+export const PostMutations = {
+  async createPost(
+    prt: any,
+    args: { message: string },
+    { req, res }: Context
+  ) {},
+  async createComment(
+    prt: any,
+    args: { message: string },
+    { req, res }: Context
+  ) {}
+};
