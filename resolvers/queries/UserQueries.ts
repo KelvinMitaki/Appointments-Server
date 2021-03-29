f67c70586f57d8ac8a8fc6a7b8c1ac6547416ae1
KelvinMitaki <mitakikelvin1@gmail.com>
2021-03-29 13:27:55 +0300

    added cl

--- resolvers/queries/UserQueries.ts
+++ resolvers/queries/UserQueries.ts
@@ -5,6 +5,7 @@ import { patientAuth } from "../../middlewares/auth";
 
 export const UserQueries = {
   async fetchCurrentUser(prt: any, args: any, { req, res }: Context) {
+    console.log(req.cookies);
     if (!req.cookies || Object.keys(req.cookies).length === 0) {
       return null;
     }
@@ -19,6 +20,7 @@ export const UserQueries = {
 
     try {
       const user = jwt.verify(split_token, process.env.JWT_SECRET!);
+      console.log(user);
       return user;
     } catch (error) {
       console.log({ error });
