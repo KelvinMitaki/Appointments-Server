4b260d5599723216551f36bc467dd97cf40ffc34
KelvinMitaki <mitakikelvin1@gmail.com>
2021-05-06 21:11:16 +0300

    refactored message broadcasting

--- resolvers/subscriptions/UserSubscriptions.ts
+++ resolvers/subscriptions/UserSubscriptions.ts
@@ -1,4 +1,5 @@
 import { PubSub, withFilter } from "graphql-subscriptions";
+import { MessageDoc } from "../../models/Message";
 
 export const pubsub = new PubSub();
 
@@ -6,8 +7,17 @@ export const UserSubscriptions = {
   addMessage: {
     subscribe: withFilter(
       () => pubsub.asyncIterator(["ADD_MESSAGE"]),
-      (payload, variables) => {
-        return true;
+      (payload, variables: { receiverID: string; senderID: string }) => {
+        let { receiver, sender }: MessageDoc = payload.addMessage;
+        let { receiverID, senderID } = variables;
+        receiver = receiver.toString();
+        sender = sender.toString();
+        receiverID = receiverID.toString();
+        senderID = senderID.toString();
+        return (
+          (receiver === receiverID && senderID === sender) ||
+          (receiver === senderID && sender === receiverID)
+        );
       }
     )
   }
