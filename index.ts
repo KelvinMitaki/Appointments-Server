3f89f3ec4c9ddf03429779d7ebe6880b2a05a463
KelvinMitaki <mitakikelvin1@gmail.com>
2021-05-18 12:49:31 +0300

    revertted

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import { ApolloServer } from "apollo-server-express";
-import os from "os";
-import cluster from "cluster";
 import mongoose from "mongoose";
 import express from "express";
 import http from "http";
@@ -8,64 +6,56 @@ import cookieParser from "cookie-parser";
 import resolvers from "./resolvers";
 import typeDefs from "./schema/schema";
 
-if (cluster.isMaster) {
-  const n_cpus = os.cpus().length;
-  console.log(`forking ${n_cpus} processes`);
-  for (let i = 0; i < n_cpus; i++) {
-    cluster.fork();
+const app = express();
+
+app.set("trust proxy", "1");
+app.use(cookieParser());
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context({ req, res }) {
+    return {
+      req,
+      res
+    };
+  },
+  subscriptions: {
+    onConnect: () => console.log("connected to websocket")
   }
-} else {
-  const app = express();
+});
 
-  app.set("trust proxy", "1");
-  app.use(cookieParser());
+const httpServer = http.createServer(app);
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context({ req, res }) {
-      return {
-        req,
-        res
-      };
-    },
-    subscriptions: {
-      onConnect: () => console.log("connected to websocket")
-    }
-  });
+server.applyMiddleware({
+  app,
+  path: "/graphql",
+  cors: {
+    credentials: true,
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://appointments-seven.vercel.app"
+        : "http://localhost:3000"
+  }
+});
 
-  const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
 
-  server.applyMiddleware({
-    app,
-    path: "/graphql",
-    cors: {
-      credentials: true,
-      origin:
-        process.env.NODE_ENV === "production"
-          ? "https://appointments-seven.vercel.app"
-          : "http://localhost:3000"
-    }
-  });
+const mongooseConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    });
+    console.log("connected to the database");
+  } catch (error) {
+    console.log(error);
+  }
+};
+mongooseConnect();
 
-  server.installSubscriptionHandlers(httpServer);
-
-  const mongooseConnect = async () => {
-    try {
-      await mongoose.connect(process.env.MONGO_URI!, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-      });
-      console.log("connected to the database");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  mongooseConnect();
-
-  httpServer.listen({ port: process.env.PORT || 4000 }, () =>
-    console.log(`server started on ${server.graphqlPath}`)
-  );
-}
+httpServer.listen({ port: process.env.PORT || 4000 }, () =>
+  console.log(`server started on ${server.graphqlPath}`)
+);
