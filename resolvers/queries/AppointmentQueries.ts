c0da583547ae9a6b32a28557c151ec26bb3a3d18
KelvinMitaki <mitakikelvin1@gmail.com>
2021-06-22 00:43:27 +0300

    wired up AppointmentQueries types

--- resolvers/queries/AppointmentQueries.ts
+++ resolvers/queries/AppointmentQueries.ts
@@ -1,13 +1,18 @@
-import { Context } from "..";
-import { patientAuth } from "../../middlewares/auth";
-import AppointMent from "../../models/Appointment";
+import { Context } from '..';
+import {
+  AppointMent as AppointMentInterface,
+  QueryResolvers,
+  ResolverTypeWrapper,
+} from '../../generated/graphql';
+import { patientAuth } from '../../middlewares/auth';
+import AppointMent from '../../models/Appointment';
 
-export const AppointmentQueries = {
-  fetchAppointMents(prt: any, args: any, { req }: Context) {
+export const AppointmentQueries: QueryResolvers<Context> = {
+  fetchAppointMents(prt, args, { req }) {
     patientAuth(req, true);
     return AppointMent.find({ date: { $gte: Date.now() } })
-      .populate("patient")
+      .populate('patient')
       .limit(50)
-      .sort({ date: 1 });
-  }
+      .sort({ date: 1 }) as unknown as ResolverTypeWrapper<AppointMentInterface>[];
+  },
 };
