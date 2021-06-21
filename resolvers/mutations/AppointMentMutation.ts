aa4f1fbf0bb9b660dda13d9f0fb17410512ae1b8
KelvinMitaki <mitakikelvin1@gmail.com>
2021-06-22 00:28:26 +0300

    wired up AppointMentMutation types

--- resolvers/mutations/AppointMentMutation.ts
+++ resolvers/mutations/AppointMentMutation.ts
@@ -1,23 +1,18 @@
-import { Context } from "..";
-import { doctorAuth } from "../../middlewares/auth";
-import AppointMent from "../../models/Appointment";
+import { Context } from '..';
+import { AppointMent as AppointMentInterface, MutationResolvers } from '../../generated/graphql';
+import { doctorAuth } from '../../middlewares/auth';
+import AppointMent from '../../models/Appointment';
 
-export const AppointMentMutation = {
-  async addAppointMent(
-    prt: any,
-    args: { patient: string; date: string },
-    { req }: Context
-  ) {
+export const AppointMentMutation: MutationResolvers<Context> = {
+  async addAppointMent(prt, args, { req }) {
     const doctor = doctorAuth(req);
     const appointMent = AppointMent.build({
       ...args,
       doctor: doctor._id,
-      date: parseInt(args.date)
+      date: parseInt(args.date),
     });
     await appointMent.save();
-    const populatedApp = AppointMent.findById(appointMent._id).populate(
-      "patient"
-    );
-    return populatedApp;
-  }
+    const populatedApp = AppointMent.findById(appointMent._id).populate('patient');
+    return populatedApp as unknown as AppointMentInterface;
+  },
 };
