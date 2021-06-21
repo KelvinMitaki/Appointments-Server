f99637c18e132d5edaa00720603afcea33152bd5
KelvinMitaki <mitakikelvin1@gmail.com>
2021-06-22 00:56:24 +0300

    wired up UserQueries types

--- resolvers/queries/UserQueries.ts
+++ resolvers/queries/UserQueries.ts
@@ -1,17 +1,23 @@
-import { Context } from "..";
-import jwt from "jsonwebtoken";
-import User from "../../models/User";
-import { patientAuth } from "../../middlewares/auth";
+import { Context } from '..';
+import jwt from 'jsonwebtoken';
+import User from '../../models/User';
+import { patientAuth } from '../../middlewares/auth';
+import {
+  Maybe,
+  QueryResolvers,
+  ResolverTypeWrapper,
+  User as UserInterface,
+} from '../../generated/graphql';
 
-export const UserQueries = {
-  async fetchCurrentUser(prt: any, args: any, { req, res }: Context) {
+export const UserQueries: QueryResolvers<Context> = {
+  async fetchCurrentUser(_, __, { req }): Promise<Maybe<ResolverTypeWrapper<UserInterface>>> {
     if (!req.cookies || Object.keys(req.cookies).length === 0) {
       return null;
     }
-    let split_token = req.cookies["client_token"];
+    let split_token = req.cookies['client_token'];
 
     if (!split_token) {
-      split_token = req.cookies["token"];
+      split_token = req.cookies['token'];
     }
     if (!split_token) {
       return null;
@@ -19,14 +25,16 @@ export const UserQueries = {
 
     try {
       const user = jwt.verify(split_token, process.env.JWT_SECRET!);
-      return user;
+      return user as unknown as Promise<Maybe<ResolverTypeWrapper<UserInterface>>>;
     } catch (error) {
       console.log({ error });
       return null;
     }
   },
-  async fetchUsers(prt: any, args: any, { req }: Context) {
+  async fetchUsers(_, __, { req }) {
     const patient = patientAuth(req, true);
-    return User.find({ _id: { $ne: patient._id }, isDoctor: false }).limit(50);
-  }
+    return User.find({ _id: { $ne: patient._id }, isDoctor: false }).limit(
+      50
+    ) as unknown as Promise<ResolverTypeWrapper<UserInterface>[]>;
+  },
 };
