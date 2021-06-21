fa960a7f39ca71a71f9a3f3c7e24e5cfa931fe9d
KelvinMitaki <mitakikelvin1@gmail.com>
2021-06-22 00:32:47 +0300

    wired up MessageMutations types

--- resolvers/mutations/MessageMutations.ts
+++ resolvers/mutations/MessageMutations.ts
@@ -1,64 +1,54 @@
-import { Context } from "..";
-import { doctorAuth, patientAuth } from "../../middlewares/auth";
-import HealthMessage from "../../models/HealthMessage";
-import Message from "../../models/Message";
-import { pubsub } from "../subscriptions/UserSubscriptions";
+import { Context } from '..';
+import {
+  HealthMessage as HealthMessageInterface,
+  Message as MessageInterface,
+  MutationResolvers,
+} from '../../generated/graphql';
+import { doctorAuth, patientAuth } from '../../middlewares/auth';
+import HealthMessage from '../../models/HealthMessage';
+import Message from '../../models/Message';
+import { pubsub } from '../subscriptions/UserSubscriptions';
 
-export const MessageMutations = {
-  async addMessage(
-    prt: any,
-    args: {
-      receiver: string;
-      message: string;
-    },
-    { req }: Context
-  ) {
+export const MessageMutations: MutationResolvers<Context> = {
+  async addMessage(prt, args, { req }) {
     const sender = patientAuth(req);
     const message = Message.build({
       ...args,
       sender: sender._id,
-      read: [sender._id]
+      read: [sender._id],
     });
     await message.save();
-    pubsub.publish("ADD_MESSAGE", {
+    pubsub.publish('ADD_MESSAGE', {
       addMessage: {
-        ...message.toObject()
-      }
+        ...message.toObject(),
+      },
     });
-    return message;
+    return message as unknown as MessageInterface;
   },
-  async readMessage(
-    prt: any,
-    args: { reader: string; messageID: string },
-    { req }: Context
-  ) {
+  async readMessage(prt, args, { req }) {
     patientAuth(req);
     const message = await Message.findById(args.messageID);
     if (message) {
-      const read = message.read.find(r => r.toString() === args.reader);
+      const read = message.read.find((r) => r.toString() === args.reader);
       if (!read) {
         message.read = [...message.read, args.reader];
       }
       await message.save();
     }
-    return message;
+    return message as unknown as MessageInterface;
   },
-  async addHealthMessage(
-    prt: any,
-    args: { title: string; body: string },
-    { req }: Context
-  ) {
+  async addHealthMessage(prt, args, { req }) {
     const doctor = doctorAuth(req);
     const healthMessage = HealthMessage.build({ ...args, doctor: doctor._id });
     await healthMessage.save();
-    return healthMessage;
+    return healthMessage as unknown as HealthMessageInterface;
   },
-  deleteHealthMessage(prt: any, args: { _id: string }, { req }: Context) {
+  deleteHealthMessage(prt, args, { req }) {
     const doc = doctorAuth(req);
     return HealthMessage.findByIdAndUpdate(
       args._id,
       { deleted: true, deletee: doc._id },
       { new: true }
-    );
-  }
+    ) as unknown as HealthMessageInterface;
+  },
 };
